d9f5a5add166dcd7d1eebeaab65d80d633935dcc
Ashwini Sanga <ashumandal96@gmail.com>
2024-03-16 10:48:15 +0530

    Changes in specs

--- specs/StringCalculator_spec.rb
+++ specs/StringCalculator_spec.rb
@@ -3,31 +3,33 @@ equire 'rspec'
 require_relative 'StringCalculator.rb'
 
 describe StringCalculator do
-  it 'Should returns 0 for an empty string' do
-    expect(add("")).to eq(0)
-  end
+    context "Invoke #add method" do
+        subject {StringCalculator.new}
+        it 'Should returns 0 for an empty string' do
+            expect(subject.add("")).to eq(0)
+        end
 
-  it 'returns the number for a single number' do
-    expect(add("1")).to eq(1)
-  end
+        it 'returns the number for a single number' do
+            expect(subject.add("1")).to eq(1)
+        end
 
-  it 'returns the sum for two numbers' do
-    expect(add("1,5")).to eq(6)
-  end
+        it 'returns the sum for two numbers' do
+            expect(subject.add("1,5")).to eq(6)
+        end
 
-  it 'handles new lines between numbers' do
-    expect(add("1\n2,3")).to eq(6)
-  end
+        it 'handles new lines between numbers' do
+            expect(subject.add("1\n2,3")).to eq(6)
+        end
 
-  it 'supports different delimiters' do
-    expect(add("//;\n1;2")).to eq(3)
-  end
+        it 'supports different delimiters' do
+            expect(subject.add("//;\n1;2")).to eq(3)
+        end
 
-  it 'throws an exception for negative numbers' do
-    expect { add("1,-2") }.to raise_error(ArgumentError, "Negative numbers not allowed: -2")
-  end
+        it 'throws an exception for negative numbers' do
+            expect { subject.add("1,-2") }.to raise_error(ArgumentError, "Negative numbers not allowed: -2")
+        end
 
-  it 'throws an exception for multiple negative numbers' do
-    expect { add("1,-2,3,-4") }.to raise_error(ArgumentError, "Negative numbers not allowed: -2, -4")
-  end
+        it 'throws an exception for multiple negative numbers' do
+            expect { subject.add("1,-2,3,-4") }.to raise_error(ArgumentError, "Negative numbers not allowed: -2, -4")
+        end
 end
