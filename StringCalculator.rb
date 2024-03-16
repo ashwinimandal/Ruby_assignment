3f714be6d9a93d243d14d0685de6cf5ea9ed62cf
Ashwini Sanga <ashumandal96@gmail.com>
2024-03-16 11:10:21 +0530

    Added complete calculate logic along with rspecs

--- StringCalculator.rb
+++ StringCalculator.rb
@@ -4,17 +4,20 @@ class StrinCalculator
         
         delimiter = /[, \n]/
         if numbers.start_with?("//")
-            delimiter, numbers = numbers.match(/^\/\/(.+)\n(.+)/).captures
+            delimiter = numbers[2]
+            numbers = numbers[4..-1]
         end
         
-        numbers = numbers.split(/#{delimiter}|\n/).map(&:to_i)
-        negatives = numbers.select { |num| num < 0 }
-        
-        if negatives.any?
-            raise ArgumentError, "Negative numbers not allowed: #{negatives.join(', ')}"
-        end
-        
-        numbers.sum
+        numbers.split(delimiter).map(&:to_i).each do |num|
+            raise "Negative numbers not allowed: #{num}" if num.negative?
+        end.reduce(:+)
           
     end
 end
+
+# Test cases
+puts StrinCalculator.add("")       # Output: 0
+puts StrinCalculator.add("1")      # Output: 1
+puts StrinCalculator.add("1,5")    # Output: 6
+puts StrinCalculator.add("1\n2,3") # Output: 6
+puts StrinCalculator.add("//;\n1;2") # Output: 3
