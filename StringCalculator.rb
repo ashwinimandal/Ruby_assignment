ca35677797dc0c6ceb5f8ed11349d001d69e8bd9
Ashwini Sanga <ashumandal96@gmail.com>
2024-03-16 10:40:06 +0530

    Added initial logic

--- StringCalculator.rb
+++ StringCalculator.rb
@@ -1,4 +1,20 @@
 class StrinCalculator
     def self.add(numbers)
+        return 0 if numbers.empty?
+        
+        delimiter = ","
+        if numbers.start_with?("//")
+            delimiter, numbers = numbers.match(/^\/\/(.+)\n(.+)/).captures
+        end
+        
+        numbers = numbers.split(/#{delimiter}|\n/).map(&:to_i)
+        negatives = numbers.select { |num| num < 0 }
+        
+        if negatives.any?
+            raise ArgumentError, "Negative numbers not allowed: #{negatives.join(', ')}"
+        end
+        
+        numbers.sum
+          
     end
 end
